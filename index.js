a1387a4b9166c8003a3f1be2293dccad4d3dfe41
Nonsticklotion <pisit676@gmail.com>
2024-07-23 23:01:01 +0700

    add public

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const errorMiddleware = require("./middlewares/error");
 const app = express();
 
 // app.use(cors());
-const corsOptions = {
-  origin: 'https://cc17-project-client.onrender.com',
-  credentials: true,
-};
 // const corsOptions = {
-//   origin: 'http://localhost:5173',
+//   origin: 'https://cc17-project-client.onrender.com',
 //   credentials: true,
 // };
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+};
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
