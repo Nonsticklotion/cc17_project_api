1d88d709c4bfd4c7795943ce10e49e7ec03a8cea
Nonsticklotion <pisit676@gmail.com>
2024-07-23 23:32:12 +0700

    fix cors option

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -2,7 +2,7 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "public/images");
+    cb(null, "publics/images");
   },
   filename: (req, file, cb) => {
     const filename = ` ${new Date().getTime()}${Math.round(
